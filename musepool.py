f4d119ef525454d75d9f0b63031a3ea213d28959
Spencer Gunning <swcgunning@gmail.com>
2017-09-21 14:12:06 -0400

    [0.0.2-a4] Print a playlist, length 51-53 minutes

--- musepool.py
+++ musepool.py
@@ -1,3 +1,4 @@
+from os import execl
 from random import shuffle
 
 def parse(song):
@@ -5,7 +6,12 @@ def parse(song):
 
 def getSec(time):
     mm, ss = time.split(':')
-    return str((int(mm) * 60) + int(ss))
+    return (int(mm) * 60) + int(ss)
+
+def INVgetSec(time):
+    mm = str(int(time / 60))
+    ss = str("%02d" % (time % 60))
+    return (mm + ':' + ss)
 
 listParsed = []
 
@@ -15,7 +21,21 @@ with open('data.mspl') as f:
         listParsed.append(parse(line))
 
 shuffle(listParsed)
+runtime = 0
 
 for song in listParsed:
     print('[' + song[0] + '] ' + song[1] + ' - ' + \
-    song[2] + ' [' + getSec(song[3]) + ']')
+    song[2] + ' [' + str(getSec(song[3])) + ']')
+    runtime += getSec(song[3])
+    if runtime >= 3180:
+        print('\x1b[1A' + '\x1b[2K' + '\x1b[1A')
+        runtime -= getSec(song[3])
+        continue
+    elif runtime >= 3060:
+        print('Total runtime: ' + INVgetSec(runtime))
+        break
+    elif song == listParsed[-1]:
+        # print('Trying again...')
+        # execl('python musepool.py')
+    else:
+        continue
