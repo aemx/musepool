8e9a530288450c78af3cf6d206b4c1dbef8df5c6
Spencer Gunning <swcgunning@gmail.com>
2017-09-21 13:28:20 -0400

    [0.0.2-a3] Print time in seconds

--- musepool.py
+++ musepool.py
@@ -3,6 +3,10 @@ from random import shuffle
 def parse(song):
     return (song.strip('\n')).split(' --- ')
 
+def getSec(time):
+    mm, ss = time.split(':')
+    return str((int(mm) * 60) + int(ss))
+
 listParsed = []
 
 with open('data.mspl') as f:
@@ -14,4 +18,4 @@ shuffle(listParsed)
 
 for song in listParsed:
     print('[' + song[0] + '] ' + song[1] + ' - ' + \
-    song[2] + ' [' + song[3] + ']')
+    song[2] + ' [' + getSec(song[3]) + ']')
