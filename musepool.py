bd131d08f79412f8be18e44131f7610c6dfac261
Spencer Gunning <swcgunning@gmail.com>
2017-09-21 15:30:05 -0400

    [0.0.2-a5] Split playlist in two

--- musepool.py
+++ musepool.py
@@ -13,29 +13,45 @@ def INVgetSec(time):
     ss = str("%02d" % (time % 60))
     return (mm + ':' + ss)
 
+def getPlaylist(lis, runtimeMax, runtimeMin):
+    playlist = []
+    runtime = 0
+
+    for idx, song in enumerate(lis):
+        print('[' + song[0] + '] ' + song[1] + ' - ' + \
+        song[2] + ' [' + str(getSec(song[3])) + ']')
+        lis.remove(song)
+        runtime += getSec(song[3])
+
+        if runtime >= runtimeMax:
+            print('\x1b[1A' + '\x1b[2K' + '\x1b[1A')
+            runtime -= getSec(song[3])
+            lis.append(song)
+            if idx == len(lis) - 1:
+                print('FAILED')
+                # restart
+                break
+            else:
+                continue
+
+        elif runtime >= runtimeMin:
+            print('[Total runtime: ' + INVgetSec(runtime) + ']')
+            break
+
+        else:
+            continue
+
 listParsed = []
 
 with open('data.mspl') as f:
     listSongs = f.readlines()
+
     for line in listSongs:
         listParsed.append(parse(line))
 
 shuffle(listParsed)
-runtime = 0
-
-for song in listParsed:
-    print('[' + song[0] + '] ' + song[1] + ' - ' + \
-    song[2] + ' [' + str(getSec(song[3])) + ']')
-    runtime += getSec(song[3])
-    if runtime >= 3180:
-        print('\x1b[1A' + '\x1b[2K' + '\x1b[1A')
-        runtime -= getSec(song[3])
-        continue
-    elif runtime >= 3060:
-        print('Total runtime: ' + INVgetSec(runtime))
-        break
-    elif song == listParsed[-1]:
-        # print('Trying again...')
-        # execl('python musepool.py')
-    else:
-        continue
+print(len(listParsed))
+getPlaylist(listParsed, 1500, 1440)
+print(len(listParsed))
+getPlaylist(listParsed, 1680, 1620)
+print(len(listParsed))
