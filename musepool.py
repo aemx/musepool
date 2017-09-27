8b0d237cc3db9b069f5ac79d90b8d3813af5d0e9
Spencer Gunning <swcgunning@gmail.com>
2017-09-27 09:32:23 -0400

    [0.0.5-a1] Added a three-choice prompt

--- musepool.py
+++ musepool.py
@@ -17,24 +17,32 @@ def INVgetSec(time):
 def traverse(key):
     num, key = re.findall('\d+|\D+', key)
     num = int(num)
+
     if num == 12:
         fNum, bNum = '1', '11'
+
     elif num == 1:
         fNum, bNum = '2', '12'
+
     else:
         fNum, bNum = str(num + 1), str(num - 1)
+
     if key == 'A':
         nKey = 'B'
+
     else:
         nKey = 'A'
+
     return [bNum + key, str(num) + key, fNum + key, str(num) + nKey]
 
 def getPlaylist(lis, runtimeMax, runtimeMin):
     e = 0
+
     while e == 0:
         shuffle(lis)
         genres, lastTraverse, playlist, returnstr = [], [], [], []
         runtime = 0
+
         for idx, song in enumerate(lis):
             returnstr.append('[' + song[4] + '] [' + \
             song[0] + '] ' + song[1] + ' - ' + song[2] + ' [' + \
@@ -42,49 +50,91 @@ def getPlaylist(lis, runtimeMax, runtimeMin):
             lis.remove(song)
             runtime += getSec(song[3])
             genres.append(song[0])
+
             if idx == 0:
                 pass
+
             else:
                 if song[4] in lastTraverse:
                     pass
+
                 else:
                     returnstr.remove('[' + song[4] + '] [' + \
                     song[0] + '] ' + song[1] + ' - ' + song[2] + ' [' + \
                     str(getSec(song[3])) + ']\n')
+                    lis.append(song)
                     runtime -= getSec(song[3])
                     genres.remove(song[0])
-                    lis.append(song)
+
                     if idx == len(lis) - 1:
                         break
+
                     else:
                         continue
+
             if runtime >= runtimeMax:
                 returnstr.remove('[' + song[4] + '] [' + \
                 song[0] + '] ' + song[1] + ' - ' + song[2] + ' [' + \
                 str(getSec(song[3])) + ']\n')
+                lis.append(song)
                 runtime -= getSec(song[3])
                 genres.remove(song[0])
-                lis.append(song)
+
                 if idx == len(lis) - 1:
                     break
+
                 else:
                     continue
+
             elif runtime >= runtimeMin:
                 returnstr.append('\n[Total runtime: ' + \
                 INVgetSec(runtime) + ']\n')
+
                 if len(genres) > 4:
                     e = 1
                     return ''.join(returnstr)
                     break
+                
                 else:
                     break
+
             elif idx == len(lis) - 1:
                 break
+
             else:
                 lastTraverse = traverse(song[4])
 
 os.system('clear')
 listParsed = []
+
+print('Please select an option:')
+print('  o - output')
+print('  i - input')
+print('  x - exit\n')
+
+while True:
+    try:
+        selectIO = input('>>> ')
+
+    except ValueError:
+        print('\nIncorrect operation specified. Please retry.\n')
+        continue
+
+    if selectIO == 'o':
+        print('\no selected\n')
+        break
+
+    elif selectIO is 'i':
+        print('\ni selected\n')
+        break
+
+    elif selectIO is 'x':
+        print('\nx selected\n')
+        break
+    
+    else:
+        print('\nIncorrect operation specified. Please retry.\n')
+        continue
 
 with open('data.mspl') as f:
     listSongs = f.readlines()
